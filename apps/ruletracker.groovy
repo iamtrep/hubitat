f2ad09246bfbd71d9cbe29b229f9003989db8b14
PJ <120332908+iamtrep@users.noreply.github.com>
2024-03-05 16:57:55 -0500

    cleanup, add results pane

--- apps/ruletracker.groovy
+++ apps/ruletracker.groovy
@@ -7,14 +7,23 @@ definition(
     iconUrl: "",
     iconX2Url: "")
 
+import hubitat.helper.RMUtils
+import groovy.util.XmlSlurper
+import groovy.transform.Field
+
+@Field static final String app_status_url = "http://127.0.0.1:8080/installedapp/status"
+
+
 preferences {
     page(name: "mainPage", title: "App parameters", install: true, uninstall: true) {
         section("Apps to track") {
             def rules = RMUtils.getRuleList("5.0")
             input name: "appIdsToTrack", type: "enum", options: rules, required: true, multiple: true, submitOnChange: true
         }
-        section("Operational parameters") {
-            input name: "scheduleCheck", type: "number", title: "Run check every N minutes", defaultValue: 5, required: true, submitOnChange: true
+
+        section(hideable:true, hidden:true, "Operational parameters") {
+            def scheduleOptions = [[0:"disabled"],[1:"1 min"],[5:"5 min"], [15:"15 min"], [30:"30 min"], [60: "1 hour"], [180:"3 hours"]]
+            input name: "scheduledCheck", type: "enum", title: "Rule check frequency", defaultValue: 5, options: scheduleOptions, required: true, submitOnChange: true
             input name: "debugLogs", type: "bool", title: "Enable debug logging?", defaultValue: false, required: true, submitOnChange: true
             input name: "traceLogs", type: "bool", title: "Enable trace logging?", defaultValue: false, required: true, submitOnChange: true
             input("hubSecurity", "bool", title: "Hub Security Enabled", defaultValue: false, submitOnChange: true, width:4)
@@ -23,13 +32,24 @@ preferences {
                 input("hubSecurityPassword", "password", title: "Hub Security Password", required: false, width:4)
             }
         }
+
         section("Actions") {
-            input name: "refreshButton", type: "button", title: "Refresh now"
+            input name: "refreshButton", type: "button", title: "Run check now"
             input name: "stopButton", type: "button", title: "Remove schedule"
             input name: "testButton", type: "button", title: "Run App Tests"
         }
+
+        section(hideable:true, "Rule Status Check - Results") {
+            paragraph(state.checkResults)
+        }
+
+        section(hideable:true, hidden:true, "App Name") {
+            input "thisName", "text", title: "Name this Rule Tracker", submitOnChange: true
+            if(thisName) app.updateLabel("$thisName")
+        }
     }
 }
+
 
 def appButtonHandler(btn) {
     switch(btn) {
@@ -48,81 +68,51 @@ def appButtonHandler(btn) {
       }
 }
 
-import hubitat.helper.RMUtils
-import groovy.util.XmlSlurper
-import groovy.transform.Field
 
 def installed() {
     if (debugLogs) log.debug "installed()"
 }
 
-@Field static final String app_status_url = "http://127.0.0.1:8080/installedapp/status"
 
 def updated() {
     if (debugLogs) log.debug "updated()"
 
-    // TODO: consider the refresh rate input
-    unschedule()
-    runEvery5Minutes("runRuleTracker")
+    updateScheduledCheck()
 }
 
+
+def uninstalled() {
+    if (debugLogs) log.debug "uninstalled()"
+}
+
+
+// The app status page has specific headers for each table
+@Field static final String settingsSectionTag = '<h5>Settings</h5>'
+@Field static final String eventsSectionTag = '<h5>Event Subscriptions</h5>'
+@Field static final String appStateSectionTag = '<h5>Application State</h5>'
+@Field static final String scheduledJobsSectionTag = '<h5>Scheduled Jobs</h5>'
+
+// Two of four tables have a table ID
+@Field static final String settingsTableId = 'settings-table'
+@Field static final String eventsTableId = 'event-table'
+@Field static final String appStateTableId = null
+@Field static final String scheduledJobsTableId = null
+
+
 def runRuleTracker() {
+    state.checkResults = ""
+
     appIdsToTrack.each {
         if (debugLogs) log.debug("checking app $it status")
         readAppStatus(it.toInteger())
     }
 }
 
+
 def testRuleTracker() {
-
-    // For now, hardcode apps to track
-    def testAppIdsToTrack = [167,412]
-
-    def fileToParse = 'response2.html'
+    def fileToParse = 'sampleAppStatus.html'
     if (debugLogs) log.debug("checking file $fileToParse")
-    //readAppStatusFromFile(fileToParse)
-
-    testAppIdsToTrack.each {
-        if (debugLogs) log.debug("checking app $it status")
-        readAppStatus(it)
-    }
-}
-
-def uninstalled() {}
-
-@Field static final String settingsSectionTag = '<h5>Settings</h5>'
-@Field static final String eventsSectionTag = '<h5>Event Subscriptions</h5>'
-@Field static final String appStateSectionTag = '<h5>Application State</h5>'
-@Field static final String scheduledJobsSectionTag = '<h5>Scheduled Jobs</h5>'
-
-@Field static final String settingsTableId = 'settings-table'
-@Field static final String eventsTableId = 'event-table'
-@Field static final String appStateTableId = null
-@Field static final String scheduledJobsTableId = null
-
-String getCookie(){
-    if (!hubSecurity) return ""
-
-    try{
-  	  httpPost(
-		[
-		uri: "http://127.0.0.1:8080",
-		path: "/login",
-		query: [ loginRedirect: "/" ],
-		body: [
-			username: hubSecurityUsername,
-			password: hubSecurityPassword,
-			submit: "Login"
-			]
-		]
-	  ) { resp ->
-		cookie = ((List)((String)resp?.headers?.'Set-Cookie')?.split(';'))?.getAt(0)
-        if(traceLogs) log.trace "$cookie"
-	  }
-    } catch (e){
-        cookie = ""
-    }
-    return "$cookie"
+    readAppStatusFromFile(fileToParse)
 }
 
 
@@ -155,7 +145,8 @@ String extractTable(String html, int startIndex = 1, String tableId = null)
     return tableHtml
 }
 
-def extractScheduledJobs(String html)
+
+def extractScheduledJobs(String html, int startIndex = 1)
 {
     //int settingsStartIndex = html.indexOf(settingsSectionTag)
     //if (settingsStartIndex == -1) log.warn("could not find settings section")
@@ -166,7 +157,7 @@ def extractScheduledJobs(String html)
     //int scheduledJobStartIndex = html.indexOf(scheduledJobsSectionTag,appStateStartIndex)
     //if (scheduledJobStartIndex == -1) log.warn("could not find scheduled job section")
 
-    def int scheduledJobStartIndex = html.indexOf(scheduledJobsSectionTag)
+    def int scheduledJobStartIndex = html.indexOf(scheduledJobsSectionTag, startIndex)
     if (scheduledJobStartIndex == -1) log.warn("could not find scheduled job section")
 
     // it's the last table in the file
@@ -180,7 +171,7 @@ def extractScheduledJobs(String html)
 }
 
 
-def extractEventSubscriptions(String html)
+def extractEventSubscriptions(String html, int startIndex = 1)
 {
     //int settingsStartIndex = html.indexOf(settingsSectionTag)
     //if (settingsStartIndex == -1) log.warn("could not find settings section")
@@ -191,7 +182,7 @@ def extractEventSubscriptions(String html)
     //int scheduledJobStartIndex = html.indexOf(scheduledJobsSectionTag)
     //if (scheduledJobStartIndex == -1) log.warn("could not find scheduled job section")
 
-    def int eventsStartIndex = html.indexOf(eventsSectionTag)
+    def int eventsStartIndex = html.indexOf(eventsSectionTag, startIndex)
     if (eventsStartIndex == -1) log.warn("could not find event subscriptions section")
 
     // it's the last table in the file
@@ -203,7 +194,6 @@ def extractEventSubscriptions(String html)
 
     return []
 }
-
 
 
 def parseTable(String tableHtml)
@@ -222,6 +212,7 @@ def parseTable(String tableHtml)
     return tableData
 }
 
+
 def readAppStatus(int appId) {
 
     def params = [
@@ -237,10 +228,14 @@ def readAppStatus(int appId) {
                     def rawHtml = resp.data.text
 
                     def tableData = extractEventSubscriptions(rawHtml)
-                    log.info("App $appId event subscriptions (${tableData.size()}) : $tableData")
+                    def result = "App $appId event subscriptions (${tableData.size()}) : $tableData"
+                    log.info(result)
+                    state.checkResults += "$result\n"
 
                     tableData = extractScheduledJobs(rawHtml)
-                    log.info("App $appId scheduled jobs (${tableData.size()}) : $tableData")
+                    result = "App $appId scheduled jobs (${tableData.size()}) : $tableData"
+                    log.info(result)
+                    state.checkResults += "$result\n"
                 }
             }
         )
@@ -249,6 +244,7 @@ def readAppStatus(int appId) {
     }
 }
 
+// For testing purposes.
 def readAppStatusFromFile(String fileName) {
     def contents = downloadHubFile(fileName)
     def html = new String(contents)
@@ -258,4 +254,68 @@ def readAppStatusFromFile(String fileName) {
 
     tableData = extractScheduledJobs(html)
     log.info("$fileName scheduled jobs (${tableData.size()}) : $tableData")
+}
+
+
+def updateScheduledCheck() {
+    unschedule()
+    switch (scheduledCheck.toInteger()) {
+        case 1:
+        runEvery1Minute("runRuleTracker")
+        break
+
+        case 5:
+        runEvery5Minutes("runRuleTracker")
+        break
+
+        case 10:
+        runEvery10Minutes("runRuleTracker")
+        break
+
+        case 15:
+        runEvery15Minutes("runRuleTracker")
+        break
+
+        case 30:
+        runEvery30Minutes("runRuleTracker")
+        break
+
+        case 60:
+        runEvery1Hour("runRuleTracker")
+        break
+
+        case 180:
+        runEvery3Hours("runRuleTracker")
+        break
+
+        case 0:
+        default:
+            break
+    }
+}
+
+
+String getCookie(){
+    if (!hubSecurity) return ""
+
+    try{
+  	  httpPost(
+		[
+		uri: "http://127.0.0.1:8080",
+		path: "/login",
+		query: [ loginRedirect: "/" ],
+		body: [
+			username: hubSecurityUsername,
+			password: hubSecurityPassword,
+			submit: "Login"
+			]
+		]
+	  ) { resp ->
+		cookie = ((List)((String)resp?.headers?.'Set-Cookie')?.split(';'))?.getAt(0)
+        if(traceLogs) log.trace "$cookie"
+	  }
+    } catch (e){
+        cookie = ""
+    }
+    return "$cookie"
 }
