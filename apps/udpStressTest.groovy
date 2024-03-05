d456b59754a0eba370ed15592e254848f4ce7f72
PJ <120332908+iamtrep@users.noreply.github.com>
2024-03-05 16:58:30 -0500

    add stop button
    todo: add some stats gathering.

--- apps/udpStressTest.groovy
+++ apps/udpStressTest.groovy
@@ -20,9 +20,23 @@ preferences {
 //            input name: "callTimeout", type: "number", title: "Call timeout in seconds", defaultValue: 30, required: true
             input name: "logThreshold", type: "number", title: "Logging threshold in ms", defaultValue: 250, required: true
             input name: "requestURL", type: "string", title: "Request IP and port (x.y.z.w:p)", defaultValue: "192.168.25.100:31337", required: true
-            input name: "debugLogs", type: "bool", title: "Enable logging?", defaultValue: true, required: true
+            input name: "debugLogs", type: "bool", title: "Enable logging?", defaultValue: false, required: true
+            input "stopButton", "button", title: "Stop current run"
+
         }
     }
+}
+
+def appButtonHandler(btn) {
+    switch(btn) {
+        case "stopButton":
+        default:
+            unschedule()
+            state.currentIteration = 0
+            state.numCallsCompleted = 0
+            state.numCallsOverThreshold = 0
+            break
+      }
 }
 
 def installed() {
@@ -32,10 +46,11 @@ def installed() {
 def updated() {
     log.info "updated() - starting a new test run"
 
+    unschedule()
     state.currentIteration = 0
     state.numCallsCompleted = 0
     state.numCallsOverThreshold = 0
-    unschedule()
+    appInstanceStats[getId()] = [:]
     //runIn(2,"udpStressTest",[misfire:"ignore"])
     schedule('*/2 * * ? * *',"udpStressTest",[misfire:"ignore"])
 }
@@ -48,6 +63,7 @@ import groovy.transform.Field
 //@Field static Long currentIteration = 0
 //@Field static Long numCallsCompleted = 0
 //@Field static Long numCallsOverThreshold = 0
+@Field static Map appInstanceStats = [:]
 
 def udpStressTest() {
     state.currentIteration++
@@ -97,12 +113,16 @@ def parseUdpMessage(message){
         BigInteger iterationNumber = parseBigInteger(hubitat.helper.HexUtils.hexStringToByteArray(params[1]))
         BigInteger callCount = parseBigInteger(hubitat.helper.HexUtils.hexStringToByteArray(params[2]))
         BigInteger timeSent = parseBigInteger(hubitat.helper.HexUtils.hexStringToByteArray(params[3]))
-        BigInteger timeEchoed = parseBigInteger(hubitat.helper.HexUtils.hexStringToByteArray(params[4]))
-        BigInteger sendTime = timeEchoed-timeSent
-        BigInteger echoTime = timeReceived-timeEchoed
+
+        // Echo server adds its own timestamp on the return, but time on client & server not synchronized well enough to make this useful
+        //BigInteger timeEchoed = parseBigInteger(hubitat.helper.HexUtils.hexStringToByteArray(params[4]))
+        //BigInteger sendTime = timeEchoed-timeSent
+        //BigInteger echoTime = timeReceived-timeEchoed
+
         BigInteger roundTripTime = timeReceived-timeSent
         logDebug("batch $batch iteration $iterationNumber of $callCount - round-trip time $roundTripTime ms")
         state.numCallsCompleted++
+
         if (roundTripTime > logThreshold) {
             state.numCallsOverThreshold++
             //log.warn("batch $batch iteration $iterationNumber of $callCount - send $sendTime ms / echo $echoTime ms / round-trip $roundTripTime ms ($numCallsOverThreshold/$numCallsCompleted)")
