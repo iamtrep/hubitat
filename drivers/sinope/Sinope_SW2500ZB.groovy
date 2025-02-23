6ad0f19a4036db2b7edebe1df8c142b0f72f500e
PJ <120332908+iamtrep@users.noreply.github.com>
2025-02-22 22:52:40 -0500

    Update Sinope_SW2500ZB.groovy

--- drivers/sinope/Sinope_SW2500ZB.groovy
+++ drivers/sinope/Sinope_SW2500ZB.groovy
@@ -451,7 +451,7 @@ private parseAttributeReport(descMap){
                 case "0090": // watt-hours delivered
                     state.energyDelivered = getEnergy(descMap.value)
                     logInfo("Energy report: ${state.energyDelivered}")
-                    return null // no event to generate
+                    return null // return directly, no event to generate
 
                     // TODO
                 case "0010": // on intensity
