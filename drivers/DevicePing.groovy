45ee2cb5d97b004e6298e75b744b2bb105516eb1
PJ <120332908+iamtrep@users.noreply.github.com>
2025-04-13 16:25:14 -0400

    driver version

--- drivers/DevicePing.groovy
+++ drivers/DevicePing.groovy
@@ -1,4 +1,30 @@
-import hubitat.helper.NetworkUtils
+/*
+
+MIT License
+
+Copyright (c) 2025
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Hubitat Elevation driver to ping devices
+
+ */
 
 metadata {
     definition (
@@ -35,11 +61,17 @@ metadata {
     }
 }
 
+import hubitat.helper.NetworkUtils
+import groovy.transform.Field
+
+@Field static final String driver_version = "0.0.1"
+
 def installed() {
     logDebug "Installed with settings: ${settings}"
     state.isPinging = false
     state.currentRetryCount = 0
     state.lastCheckin = null
+    state.version = driver_version
     state.retryThreshold = settings.retryThreshold ?: 3
     initialize()
 }
@@ -55,6 +87,12 @@ def updated() {
 }
 
 def initialize() {
+    logDebug("initialize()")
+    if (state.version != driver_version) {
+        log.warn("New driver version installed ${driver_version} (previous: ${state.version}")
+        state.version = driver_version
+    }
+
     unschedule(scheduledPing)
 
     if (deviceIP || httpURL) {
