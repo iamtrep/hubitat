8106326faba4552baaf07d3fe89824a87bd94fce
PJ <120332908+iamtrep@users.noreply.github.com>
2025-05-19 19:07:17 -0400

    Update StartupShutdownMonitor.groovy

--- apps/StartupShutdownMonitor.groovy
+++ apps/StartupShutdownMonitor.groovy
@@ -1,35 +1,35 @@
-/**
+/*
 
-MIT License
+ MIT License
 
-Copyright (c) 2025 pj
+ Copyright (c) 2025 pj
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
 
-Startup and Shutdown Monitor
+ Startup and Shutdown Monitor
 
-Description: This app monitors system events to detect when the hub is shutting down/starting up.
+ Description: This app monitors system events to detect when the hub is shutting down/starting up.
  - Opens the selected virtual contact sensor on: manualReboot, manualShutdown, update
  - Closes the selected virtual contact sensor on: systemStart
 
-The virtual contact sensor can be used in automations (e.g. Rule Machine - can be
-used as a Required Expression or condtion for triggers).
+ The virtual contact sensor can be used in automations (e.g. Rule Machine - can be
+ used as a Required Expression or condtion for triggers).
 
  */
 
@@ -55,15 +55,48 @@ preferences {
     page(name: "mainPage")
 }
 
+
+@Field static final List<String> constLocationEvents = [
+    "cloudBackup",
+    "deviceJoin",
+    "lowMemory",
+    "manualReboot",
+    "manualShutdown"
+    "severeLoad",
+    "sunrise",
+    "sunriseSunsetUpdated",
+    "sunriseTime",
+    "sunset",
+    "sunsetTime",
+    "systemStart",
+    "update",
+    "zigbeeOn",
+    "zigbeeOff",
+    "zigbeeStatus",
+    "zwaveCrashed",
+    "zwaveStatus"
+]
+
 Map mainPage() {
     dynamicPage(name: "mainPage", title: "${app.getLabel()} Setup", install: true, uninstall: true) {
         section("Select Virtual Contact Sensor") {
-            input("contactSensor", "capability.contactSensor", title: "Virtual Contact Sensor", multiple:false, required:true, showFilter:true)
-            paragraph("<a href='/device/addDevice' target='_blank'>Click here</a> to create a new Virtual Contact Sensor for use with this app")
+            input name: "contactSensor", "capability.contactSensor", title: "Virtual Contact Sensor", multiple:false, required:true, showFilter:true
+            paragraph "<a href='/device/addDevice' target='_blank'>Click here</a> to create a new Virtual Contact Sensor for use with this app"
+        }
+        section("Settings") {
+            input name: "startupDelay", title: "Wait this many seconds after systemStartup event to close the contact sensor", type: "number", defaultValue: 0, range: "0..3600", required: true
+        }
+        section(true, true, "Advanced") {
+             input "triggerEventsOpen", "enum", title: "Events to OPEN the device",
+                options: constLocationEvents, required: false, multiple: true, defaultValue: ["manualReboot","manualShutdown","update"]
+
+             input "triggerEventsClose", "enum", title: "Events to CLOSE the device",
+                options: constLocationEvents, required: false, multiple: true, defaultValue: ["systemStart"]
+
         }
         section("Logging") {
             input name: "logLevel", type: "enum", options: ["warn","info","debug","trace"], title: "Enable logging?", defaultValue: "info", required: true, submitOnChange: true
-            if (logLevel != null) logInfo("${app.getLabel()}: ${logLevel} logging enabled")
+            if (logLevel != null) logInfo("${logLevel} logging enabled")
         }
     }
 }
@@ -71,42 +104,33 @@ Map mainPage() {
 // runs when the app is first installed
 void installed() {
     initialize()
-    logDebug "${app.getLabel()} installed"
+    logDebug "installed()"
 }
 
 // runs whenever app preferences are saved (click Done on app config page)
 void updated() {
+    logDebug "updated()"
     unsubscribe()
-    initialize()
-    logDebug "${app.getLabel()} updated"
+    subscribe(location, "eventHandler")
+    logDebug "${contactSensor?.getDisplayName()} ${contactSensor?.currentValue('contact')}"
 }
 
 void initialize() {
-    subscribe(location, "manualReboot", "eventHandler")
-    subscribe(location, "manualShutdown", "eventHandler")
-    subscribe(location, "update", "eventHandler")
-    subscribe(location, "systemStart", "eventHandler")
-
-    logDebug "${app.getLabel()} initialized"
-    logDebug "${app.getLabel()} ${contactSensor?.getDisplayName()} ${contactSensor?.currentValue('contact')}"
+    logDebug "initialize()"
 }
 
 void eventHandler(Event evt) {
     logDebug "System event detected: ${evt.name}"
 
-    switch(evt.name) {
-        case "manualReboot":
-        case "manualShutdown":
-        case "update":
-            openContact(evt.descriptionText)
-            break
-        case "systemStart":
-            closeContact(evt.descriptionText)
-            break
-        default:
-            logWarn "Unhandled event: ${evt.name}"
-            break
+    if (evt.name in triggerEventsOpen) {
+        openContact(evt.descriptionText)
     }
+
+    if (evt.name in triggerEventsClose) {
+        closeContact(evt.descriptionText)
+    }
+
+    logTrace("Unhandled location event: ${evt.name}")
 }
 
 void openContact(String message) {
@@ -127,30 +151,31 @@ void closeContact(String message) {
     contactSensor?.close()
 }
 
+// logging helpers
 
-private void logError(Object... args)
+private void logError(String msg)
 {
     //if (logLevel in ["info","debug","trace"])
-    log.error(args)
+    log.error(app.getLabel() + ': ' + msg)
 }
 
-private void logWarn(Object... args)
+private void logWarn(String msg)
 {
     //if (logLevel in ["warn", "info","debug","trace"])
-    log.warn(args)
+    log.warn(app.getLabel() + ': ' + msg)
 }
 
-private void logInfo(Object... args)
+private void logInfo(String msg)
 {
-    if (loglevel == null || logLevel in ["info","debug","trace"]) log.info(args)
+    if (logLevel == null || logLevel in ["info","debug","trace"]) log.info(app.getLabel() + ': ' + msg)
 }
 
-private void logDebug(Object... args)
+private void logDebug(String msg)
 {
-    if (logLevel == null || logLevel in ["debug","trace"]) log.debug(args)
+    if (logLevel == null || logLevel in ["debug","trace"]) log.debug(app.getLabel() + ': ' + msg)
 }
 
-private void logTrace(Object... args)
+private void logTrace(String msg)
 {
-    if (logLevel == null || logLevel in ["trace"]) log.trace(args)
+    if (logLevel == null || logLevel in ["trace"]) log.trace(app.getLabel() + ': ' + msg)
 }
