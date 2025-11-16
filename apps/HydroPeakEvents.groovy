d2b4320c19ee339ec469dbc8ed9272b437a74977
PJ <120332908+iamtrep@users.noreply.github.com>
2025-11-15 19:22:48 -0500

    Update HydroPeakEvents.groovy

--- apps/HydroPeakEvents.groovy
+++ apps/HydroPeakEvents.groovy
@@ -1,12 +1,30 @@
-/**
- *  Hydro-Québec Peak Period Manager
- *
- *  Manages thermostats and appliances during Hydro-Québec peak periods
- *  Implemented as a state machine with four states:
- *  - NO_EVENTS: No upcoming events
- *  - EVENT_SCHEDULED: Event scheduled but not imminent
- *  - PRE_EVENT: Within warning period before event
- *  - EVENT_ACTIVE: Event is currently happening
+/*
+ MIT License
+
+ Copyright (c) 2025 pj
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Hydro-Québec Peak Period Manager
+
+ A simple state machine-based app to help Manage thermostats and appliances during Hydro-Québec peak periods
+
  */
 
 import groovy.transform.CompileStatic
