f9a65f5a9675db11fecacc860d8673b26f488384
PJ <120332908+iamtrep@users.noreply.github.com>
2026-06-03 22:01:34 -0400

    WSDCGQ11LM: prune unused inlined BirdsLikeWires library code

--- drivers/XiaomiAqaraWSDCGQ11LM.groovy
+++ drivers/XiaomiAqaraWSDCGQ11LM.groovy
@@ -224,7 +224,6 @@ void processMap(Map map) {
 
 		} else {
 
-			// processBasic(map)
 			filterThis(map)
 
 		}
@@ -346,53 +345,6 @@ void push(buttonId) {
 }
 
 
-void doubleTap(buttonId) {
-	
-	sendEvent(name:"doubleTapped", value: buttonId, isStateChange:true)
-	
-}
-
-
-void hold(buttonId) {
-	
-	sendEvent(name:"held", value: buttonId, isStateChange:true)
-	
-}
-
-
-void release(buttonId) {
-	
-	sendEvent(name:"released", value: buttonId, isStateChange:true)
-	
-}
-
-//////////////// So, this may well be different for different lamps and dimmers, so... possibly not for here, 
-
-// void setLevel(BigDecimal level) {
-
-// 	setLevel(level,1)
-
-// }
-
-
-// void setLevel(BigDecimal level, BigDecimal duration) {
-
-// 	BigDecimal safeLevel = level <= 100 ? level : 100
-// 	safeLevel = safeLevel < 0 ? 0 : safeLevel
-
-// 	String hexLevel = percentageToHex(safeLevel.intValue())
-
-// 	BigDecimal safeDuration = duration <= 25 ? (duration*10) : 255
-// 	String hexDuration = Integer.toHexString(safeDuration.intValue())
-
-// 	String pluralisor = duration == 1 ? "" : "s"
-// 	logging("${device} : setLevel : Got level request of '${level}' (${safeLevel}%) [${hexLevel}] changing over '${duration}' second${pluralisor} (${safeDuration} deciseconds) [${hexDuration}].", "debug")
-
-// 	sendEvent(name: "level", value: "${safeLevel}")
-
-// }
-
-
 void updatePresence() {
 
 	long millisNow = new Date().time
@@ -447,109 +399,6 @@ void checkPresence() {
 	} else {
 
 		logging("${device} : Presence : Waiting for first presence report.", "warn")
-
-	}
-
-}
-
-
-void processBasic(Map map) {
-	// Process the basic descriptors normally received from Zigbee Cluster 0000 into device data values.
-
-	if (map.attrId == "0001") {
-
-		updateDataValue("application", "${map.value}")
-		logging("${device} : Application : ${map.value}", "debug")
-
-	} else if (map.attrId == "0004") {
-
-		updateDataValue("manufacturer", map.value)
-		logging("${device} : Manufacturer : ${map.value}", "debug")
-
-	} else if (map.attrId == "0005") {
-
-		updateDataValue("model", map.value)
-		logging("${device} : Model : ${map.value}", "debug")
-
-	} else if (map.attrId == "4000") {
-
-		updateDataValue("softwareBuild", "${map.value}")
-		logging("${device} : Firmware : ${map.value}", "debug")
-
-	}
-
-}
-
-
-void processConfigurationResponse(Map map) {
-
-	if (map.command == "07") {
-
-		if (map.data[0] == "00") {
-
-			sendEvent(name: "configuration", value: "received", isStateChange: false)
-			logging("${device} : Configuration : Received by device.", "info")
-
-		} else {
-
-			logging("${device} : Configuration : Device may not have processed configuration correctly.", "warn")
-
-		}
-
-	} else {
-
-		reportToDev(map)
-
-	}
-
-}
-
-
-void processDescriptors(Map map) {
-	// Process the simple descriptors normally received from Zigbee Cluster 8004 into device data values.
-
-	if (map.data[1] == "00") {
-		// Received simple descriptor data.
-
-		//updateDataValue("endpointId", map.data[5])						// can lead to a weird duplicate
-		updateDataValue("profileId", map.data[6..7].reverse().join())
-
-		Integer inClusterNum = Integer.parseInt(map.data[11], 16)
-		Integer position = 12
-		Integer positionCounter = null
-		String inClusters = ""
-		if (inClusterNum > 0) {
-			(1..inClusterNum).each() {b->
-				positionCounter = position+((b-1)*2)
-				inClusters += map.data[positionCounter..positionCounter+1].reverse().join()
-				if (b < inClusterNum) {
-					inClusters += ","
-				}
-			}
-		}
-		position += inClusterNum*2
-		Integer outClusterNum = Integer.parseInt(map.data[position], 16)
-		position += 1
-		String outClusters = ""
-		if (outClusterNum > 0) {
-			(1..outClusterNum).each() {b->
-				positionCounter = position+((b-1)*2)
-				outClusters += map.data[positionCounter..positionCounter+1].reverse().join()
-				if (b < outClusterNum) {
-					outClusters += ","
-				}
-			}
-		}
-
-		updateDataValue("inClusters", inClusters)
-		updateDataValue("outClusters", outClusters)
-
-		logging("${device} : Received $inClusterNum inClusters : $inClusters", "debug")
-		logging("${device} : Received $outClusterNum outClusters : $outClusters", "debug")
-
-	} else {
-
-		reportToDev(map)
 
 	}
 
@@ -619,199 +468,10 @@ void reportToDev(map) {
 }
 
 
-@Field static Boolean debouncingParentState = false
-void debounceParentState(String attribute, String state, String message, String level, Integer duration) {
-
-	if (debouncingParentState) return
-	debouncingParentState = true
-
-	sendEvent(name: "$attribute", value: "$state")
-	logging("${device} : $message", "$level")
-
-	pauseExecution duration
-	debouncingParentState = false
-
-}
-
-
-def fetchChild(String namespace, String type, String endpoint) {
-	// Creates and retrieves child devices matched to endpoints.
-
-	// Namespace is required for custom child drivers. Use "hubitat" for system drivers.
-	// Type will determine the driver to use.
-	// Endpoint is any identifier unique to the parent.
-
-	def childDevice = getChildDevice("${device.id}-${endpoint}")
-
-	if (endpoint != "null") {
-
-		if (!childDevice) {
-
-			logging("${device} : Creating child device $device.id-$endpoint", "debug")
-
-			childDevice = addChildDevice("${namespace}", "${type}", "${device.id}-${endpoint}", [name: "${type}", label: "${endpoint}", isComponent: false])
-
-			if (type.indexOf('Switch') >= 0) {
-
-				// Presume a switch to be off until told otherwise.
-				childDevice.parse([[name: "switch", value: 'off']])
-
-			}
-
-			//childDevice.updateSetting("txtEnable", false)
-
-		}
-
-		logging("${device} : Retrieved child device $device.id-$endpoint", "debug")
-
-	} else {
-
-		logging("${device} : Received null endpoint for device $device.id", "error")
-
-	}
-
-	return childDevice
-
-}
-
-
-def fetchChildStates(String state, String requestor) {
-	// Retrieves requested states of child devices.
-
-	logging("${device} : fetchChildStates() : Called by $requestor", "debug")
-
-	def childStates = []
-	def children = getChildDevices()
-
-	children.each {child->
-
-		// Give things a chance!
-		pauseExecution(100)
-	
-		// Grabs the requested state from the child device.
-		String childState = child.currentValue("${state}")
-
-		if ("${child.id}" != "${requestor}") {
-			// Don't include the requestor's state in the results, as we're likely in the process of updating it.
-			childStates.add("${childState}")
-			logging("${device} : fetchChildStates() : Found $child.id is '$childState'", "debug")
-		}
-
-	}
-
-	return childStates
-
-}
-
-
-void deleteChildren() {
-	// Deletes children we may have created.
-
-	logging("${device} : deleteChildren() : Deleting rogue children.", "debug")
-
-	def children = getChildDevices()
-    children.each {child->
-  		deleteChildDevice(child.deviceNetworkId)
-    }
-
-}
-
-
-void componentRefresh(com.hubitat.app.DeviceWrapper childDevice) {
-
-	logging("componentRefresh() from $childDevice.deviceNetworkId", "debug")
-	sendZigbeeCommands(["he rattr 0x${device.deviceNetworkId} 0x${childDevice.deviceNetworkId.split("-")[1]} 0x0006 0x00 {}"])
-
-}
-
-
-void componentOn(com.hubitat.app.DeviceWrapper childDevice) {
-
-	logging("componentOn() from $childDevice.deviceNetworkId", "debug")
-	sendZigbeeCommands(["he cmd 0x${device.deviceNetworkId} 0x${childDevice.deviceNetworkId.split("-")[1]} 0x0006 0x01 {}"])
-
-}
-
-
-void componentOff(com.hubitat.app.DeviceWrapper childDevice) {
-
-	logging("componentOff() from $childDevice.deviceNetworkId", "debug")
-	sendZigbeeCommands(["he cmd 0x${device.deviceNetworkId} 0x${childDevice.deviceNetworkId.split("-")[1]} 0x0006 0x00 {}"])
-
-}
-
-
-private String flipLittleEndian(Map map, String attribute) {
-
-	String bigEndianAttribute = ""
-	for (int v = map."${attribute}".length(); v > 0; v -= 2) {
-		bigEndianAttribute += map."${attribute}".substring(v - 2, v)
-	}
-	return bigEndianAttribute
-
-}
-
-
-private String[] millisToDhms(BigInteger millisToParse) {
-
-	long secondsToParse = millisToParse / 1000
-
-	def dhms = []
-	dhms.add(secondsToParse % 60)
-	secondsToParse = secondsToParse / 60
-	dhms.add(secondsToParse % 60)
-	secondsToParse = secondsToParse / 60
-	dhms.add(secondsToParse % 24)
-	secondsToParse = secondsToParse / 24
-	dhms.add(secondsToParse % 365)
-	return dhms
-
-}
-
-
-private String percentageToHex(Integer pc) {
-
-	BigDecimal safePc = pc > 0 ? (pc*2.55) : 0
-	safePc = safePc > 255 ? 255 : safePc
-	return Integer.toHexString(safePc.intValue())
-
-}
-
-
 private BigDecimal hexToBigDecimal(String hex) {
 
     int d = Integer.parseInt(hex, 16) << 21 >> 21
     return BigDecimal.valueOf(d)
-
-}
-
-
-private String hexToBinary(String thisByte, Integer size = 8) {
-
-	String binaryValue = new BigInteger(thisByte, 16).toString(2);
-	return String.format("%${size}s", binaryValue).replace(' ', '0')
-	
-}
-
-
-private Integer hexToPercentage(String hex) {
-
-	String safeHex = hex.take(2)
-    Integer pc = Integer.parseInt(safeHex, 16) << 21 >> 21
-	return pc / 2.55
-
-}
-
-
-private String hexToText(String hex) {
-
-	String text = ""
-	int pos = 0
-	while(pos < hex.length() - 1) {
-		text = text + (char)Integer.parseInt(hex.substring(pos, pos + 2), 16)
-		pos += 2
-	}
-	return text.trim()
 
 }
 
@@ -887,7 +547,7 @@ void filterThis(Map map) {
 
 	if (map.clusterId == "0001") {
 
-		processConfigurationResponse(map)
+		logging("${device} : Skipped : Power Configuration Response", "debug")
 
 	} else if (map.clusterId == "0006") {
 
@@ -899,11 +559,11 @@ void filterThis(Map map) {
 
 	} else if (map.clusterId == "0400") {
 
-		processConfigurationResponse(map)
+		logging("${device} : Skipped : Illuminance Response", "debug")
 
 	} else if (map.clusterId == "8004") {
-		
-		processDescriptors(map)
+
+		logging("${device} : Skipped : Simple Descriptor Response", "debug")
 
 	} else if (map.clusterId == "8005") {
 
